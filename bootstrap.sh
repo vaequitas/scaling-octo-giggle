cacefc5bb2779847e8c62cdfc192d12659f16cbb
vaequitas <thomas@flack.it>
2018-02-24 18:45:25 +0000

    Remove golang from bootstrap, move to puppet

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@ yum update -y
 yum install -y epel-release
 
 yum install -y net-tools
-yum install -y golang
 
 readonly PUPPET_YUM_REPO="https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm"
 rpm -ivh "${PUPPET_YUM_REPO}"
