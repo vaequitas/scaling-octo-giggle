f3c13f0a3b396e19d3be3d3f12a6b462e03f82f4
vaequitas <thomas@flack.it>
2018-02-24 12:55:44 +0000

    Install puppet on vm with shell provisioner

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,7 @@ yum update -y
 
 yum install -y net-tools
 yum install -y golang
+
+readonly PUPPET_YUM_REPO="https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm"
+rpm -ivh "${PUPPET_YUM_REPO}"
+yum install -y puppet
